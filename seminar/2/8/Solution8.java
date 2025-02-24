c5cd80a1ce643924dde2469120b03905e531ba6d
Гордеев Павел Васильевич <p.v.gordeev@tinkoff.ru>
2025-02-24 20:20:47 +0400

    part of seminar 2 tasks

--- seminar/2/8/Solution8.java
+++ seminar/2/8/Solution8.java
@@ -2,38 +2,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-public class MultiStack {
-    private List<Stack<Integer>> stacks = new ArrayList<>();
-    private int totalElements = 0;
+class Solution8 {
 
-    public void push(int value) {
-        int currentStack = 0;
-        Stack<Integer> current = getStack(currentStack);
-        current.push(value);
-        totalElements++;
+    static class MultiStack {
+        private List<Stack<Integer>> stacks = new ArrayList<>();
+        private int totalElements = 0;
 
-        // Проверка на переполнение и перекладка
-        while (current.size() > Math.pow(2, currentStack)) {
-            Stack<Integer> nextStack = getStack(currentStack + 1);
-            while (!current.isEmpty()) {
-                nextStack.push(current.pop());
+        public void push(int value) {
+            int currentStack = 0;
+            Stack<Integer> current = getStack(currentStack);
+            current.push(value);
+            totalElements++;
+
+            // Проверка на переполнение и перекладка
+            while (current.size() > Math.pow(2, currentStack)) {
+                Stack<Integer> nextStack = getStack(currentStack + 1);
+                while (!current.isEmpty()) {
+                    nextStack.push(current.pop());
+                }
+                currentStack++;
+                current = nextStack;
             }
-            currentStack++;
-            current = nextStack;
+        }
+
+        private Stack<Integer> getStack(int index) {
+            if (index >= stacks.size()) {
+                stacks.add(new Stack<>());
+            }
+            return stacks.get(index);
+        }
+
+        public static void main(String[] args) {
+            MultiStack ms = new MultiStack();
+            for (int i = 0; i < 10; i++) {
+                ms.push(i);
+            }
         }
     }
 
-    private Stack<Integer> getStack(int index) {
-        if (index >= stacks.size()) {
-            stacks.add(new Stack<>());
-        }
-        return stacks.get(index);
-    }
-
-    public static void main(String[] args) {
-        MultiStack ms = new MultiStack();
-        for (int i = 0; i < 10; i++) {
-            ms.push(i);
-        }
-    }
 }
--- seminar/2/9/Solution9.java
+++ seminar/2/9/Solution9.java
@@ -1,16 +1,26 @@
-public void add(int value) {
-    int currentStack = 0;
-    while (currentStack < stacks.size() && stacks.get(currentStack).size() == (1 << currentStack)) {
-        List<Integer> nextStack = new ArrayList<>();
-        if (currentStack + 1 < stacks.size()) {
-            nextStack = stacks.get(currentStack + 1);
-        } else {
-            stacks.add(nextStack);
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution9 {
+
+    private List<List<Integer>> stacks = new ArrayList<>();
+
+    public void add(int value) {
+        int currentStack = 0;
+        while (currentStack < stacks.size() && stacks.get(currentStack).size() == (1 << currentStack)) {
+            List<Integer> nextStack = new ArrayList<>();
+            if (currentStack + 1 < stacks.size()) {
+                nextStack = stacks.get(currentStack + 1);
+            } else {
+                stacks.add(nextStack);
+            }
+            nextStack.addAll(stacks.get(currentStack));
+            Collections.sort(nextStack);
+            stacks.set(currentStack + 1, nextStack);
+            currentStack++;
         }
-        nextStack.addAll(stacks.get(currentStack));
-        Collections.sort(nextStack);
-        stacks.set(currentStack + 1, nextStack);
-        currentStack++;
+        stacks.get(currentStack).add(value);
     }
-    stacks.get(currentStack).add(value);
+
 }
